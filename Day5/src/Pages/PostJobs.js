0df209f712028fb787bf55233220ab952303ea8c
Siva k <113882773+Siva-sk2004@users.noreply.github.com>
2023-07-19 14:41:08 +0530

    Add files via upload

---
+++ Day5/src/Pages/PostJobs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostJobs = () => {
+  return (
+    <div>
+      <h4 style={{textAlign:'center' ,paddingTop:'60px'}}>Waiting for Jobs Application</h4>
+    </div>
+  )
+}
+
+export default PostJobs
